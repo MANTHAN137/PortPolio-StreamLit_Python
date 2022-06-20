5d6eeeaeb18714d99bbbcba7be1265b326a39a8a
Manthan Gopal Dhole <90518833+MANTHAN137@users.noreply.github.com>
2022-06-21 00:53:12 +0530

    Update resume.py

--- resume.py
+++ resume.py
@@ -88,8 +88,6 @@ def txt4(a, b, c):
 st.markdown('''
 ## Education
 ''')
-txt('SCC        92.8%')
-txt('HSC        78.3%')
 txt('**MHTCET** score **99.62%tile**')
 txt('**JEE**    score **96.32%tile**')
 txt('**B.Tech** Information Technology, *VJTI*, mumbai',
